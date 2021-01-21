1043f4b8aed8bfe9a9102ad2a967baa8c4cd6449
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-01-21 15:03:27 +0900

    02 EditText 추가

--- app/src/main/java/com/example/textview_01/MainActivity.java
+++ app/src/main/java/com/example/textview_01/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.textview_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et_id;
+    Button btn_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_id = findViewById(R.id.et_id);
+        btn_id = findViewById(R.id.btn_id);
+
+        btn_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_id.setText("You clicked button!");
+            }
+        });
     }
 }
