0246e97ee2bea4f82d41e95fb97bbe32c7efde81
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-01-25 15:26:31 +0900

    17 Service background music 추가

--- app/src/main/java/com/example/textview_01/SubActivity.java
+++ app/src/main/java/com/example/textview_01/SubActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
     private TextView tv_sub;
+    Button btn_musicstart, btn_musicstop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,22 @@ public class SubActivity extends AppCompatActivity {
         String str = intent.getStringExtra("str");
 
         tv_sub.setText("입력한것: "+str);
+
+        btn_musicstart = (Button)findViewById(R.id.btn_musicstart);
+        btn_musicstop = (Button)findViewById(R.id.btn_musicstop);
+
+        btn_musicstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent((getApplicationContext()),MusicService.class));
+            }
+        });
+
+        btn_musicstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent((getApplicationContext()),MusicService.class));
+            }
+        });
     }
 }
